93f80b9db99de0232de6865e0686185d3008ef5e
Jeroen <jjansen@gmail.com>
2026-08-22 08:33:08 +0200

    test(boiler): cover unreachable CM100 flow

--- tests/host/boiler_commissioning_logic_test.cpp
+++ tests/host/boiler_commissioning_logic_test.cpp
@@ -106,6 +106,43 @@ void test_apply_policy() {
   assert(!result_apply_allowed(true, false, true));
 }
 
+void test_unreachable_flow_after_sustained_saturation() {
+  FlowReachabilityMonitor monitor;
+  assert(!monitor.update(1000, 700.0f, 800.0f, 40.0f, 50.0f));
+  assert(!monitor.update(31000, 704.0f, 800.0f, 40.0f, 50.0f));
+  assert(monitor.update(61000, 703.0f, 800.0f, 40.0f, 50.0f));
+  assert(monitor.best_flow_lph() == 704.0f);
+}
+
+void test_reachability_monitor_allows_meaningful_progress() {
+  FlowReachabilityMonitor monitor;
+  assert(!monitor.update(1000, 680.0f, 800.0f, 40.0f, 55.0f));
+  assert(!monitor.update(31000, 690.0f, 800.0f, 40.0f, 55.0f));
+  assert(!monitor.update(61000, 701.0f, 800.0f, 40.0f, 55.0f));
+  assert(!monitor.update(91000, 715.0f, 800.0f, 40.0f, 55.0f));
+  assert(!monitor.update(121000, 725.0f, 800.0f, 40.0f, 55.0f));
+}
+
+void test_reachability_monitor_resets_when_actuator_not_saturated() {
+  FlowReachabilityMonitor monitor;
+  assert(!monitor.update(1000, 700.0f, 800.0f, 40.0f, 50.0f));
+  assert(!monitor.update(31000, 704.0f, 800.0f, 40.0f, 100.0f));
+  assert(!monitor.update(91000, 704.0f, 800.0f, 40.0f, 50.0f));
+}
+
+void test_reachability_monitor_resets_when_target_band_reached() {
+  FlowReachabilityMonitor monitor;
+  assert(!monitor.update(1000, 700.0f, 800.0f, 40.0f, 50.0f));
+  assert(!monitor.update(31000, 765.0f, 800.0f, 40.0f, 50.0f));
+  assert(!monitor.update(91000, 700.0f, 800.0f, 40.0f, 50.0f));
+}
+
+void test_reachability_monitor_rejects_invalid_flow() {
+  FlowReachabilityMonitor monitor;
+  assert(!monitor.update(1000, NAN, 800.0f, 40.0f, 50.0f));
+  assert(!monitor.update(61000, NAN, 800.0f, 40.0f, 50.0f));
+}
+
 }  // namespace
 
 int main() {
@@ -120,5 +157,10 @@ int main() {
   test_dynamic_flow_after_initial_800_preflow();
   test_very_high_theoretical_flow_is_limited_not_refused();
   test_apply_policy();
+  test_unreachable_flow_after_sustained_saturation();
+  test_reachability_monitor_allows_meaningful_progress();
+  test_reachability_monitor_resets_when_actuator_not_saturated();
+  test_reachability_monitor_resets_when_target_band_reached();
+  test_reachability_monitor_rejects_invalid_flow();
   return 0;
 }
