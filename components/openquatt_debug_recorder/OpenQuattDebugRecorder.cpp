288d48a43e4a4b53a9ad8032abc5a1e7fd904569
Jeroen <jjansen@gmail.com>
2026-09-01 20:09:12 +0200

    Filter klok uit debug event counter

--- components/openquatt_debug_recorder/OpenQuattDebugRecorder.cpp
+++ components/openquatt_debug_recorder/OpenQuattDebugRecorder.cpp
@@ -1012,7 +1012,7 @@ void OpenQuattDebugRecorder::capture_sample_() {
     }
     if (previous != nullptr && read_value_(previous, field) != value) {
       change_count++;
-      if (event_type_(field.type)) {
+      if (event_type_(field.type) && std::strcmp(field.key, "timeNowHhmm") != 0) {
         event_count++;
       }
     }
