8d5c287540291d63d41fe37c55bb9d1ea04333a8
Jeroen <jjansen@gmail.com>
2026-09-11 16:52:17 +0200

    Simplify controlled OTA handoff runtime

--- components/openquatt_incident_manager/OpenQuattRestartHandoff.h
+++ components/openquatt_incident_manager/OpenQuattRestartHandoff.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
 
 #include "esphome/core/component.h"
 #ifdef USE_OTA_STATE_LISTENER
 #include "esphome/components/ota/ota_backend.h"
 #endif
-#include "OpenQuattRestartHandoffPolicy.h"
 
 namespace esphome::openquatt_incident_manager {
 
@@ -33,22 +31,15 @@ class OpenQuattOtaHandoff : public Component
   void set_minimum_off_ms(uint32_t value) { this->minimum_off_ms_ = value; }
 
   void setup() override;
-  void loop() override;
 #ifdef USE_OTA_STATE_LISTENER
   void on_ota_global_state(ota::OTAState state, float progress, uint8_t error, ota::OTAComponent* component) override;
 #endif
 
  protected:
-  static constexpr uint32_t SAMPLE_INTERVAL_MS = 1000U;
-  static constexpr uint32_t FULL_CREDIT_STABLE_MS = 15000U;
-
-  bool hp_eligible_for_full_credit_(uint8_t hp_index, uint32_t now_ms) const;
-  void sample_(uint32_t now_ms);
+  uint32_t full_credit_if_confirmed_(uint8_t hp_index, uint32_t now_ms) const;
 
   OpenQuattIncidentManager* incident_manager_{nullptr};
   uint32_t minimum_off_ms_{240000U};
-  uint32_t last_sample_ms_{0U};
-  std::array<restart_handoff::StableFullCreditLatch, 2U> full_credit_latches_{};
   bool ota_handoff_attempted_{false};
   bool ota_handoff_saved_{false};
 };
