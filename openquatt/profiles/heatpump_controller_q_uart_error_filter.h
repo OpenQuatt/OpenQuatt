01dc63091aeb0b38e52f844c33ce615190635c49
Jeroen <jjansen@gmail.com>
2026-09-12 09:48:18 +0200

    Apply UART error discard to all Controller Q UARTs

--- openquatt/profiles/heatpump_controller_q_uart_error_filter.h
+++ openquatt/profiles/heatpump_controller_q_uart_error_filter.h
@@ -2,24 +2,24 @@
 
 #ifdef USE_ESP_IDF
 
+#include "driver/uart.h"
 #include "soc/uart_reg.h"
-#include "esphome/components/uart/uart_component_esp_idf.h"
 #include "esphome/core/log.h"
 
 namespace oq_controller_q_uart {
 
-inline void enable_error_character_discard(esphome::uart::IDFUARTComponent* uart, const char* port_name) {
-  const uint8_t uart_num = uart->get_hw_serial_number();
-  REG_SET_BIT(UART_CONF0_REG(uart_num), UART_ERR_WR_MASK);
+inline void enable_error_character_discard_all() {
+  for (uint8_t uart_num = 0; uart_num < UART_NUM_MAX; uart_num++) {
+    REG_SET_BIT(UART_CONF0_REG(uart_num), UART_ERR_WR_MASK);
 
-  const uint32_t conf0 = REG_READ(UART_CONF0_REG(uart_num));
-  if ((conf0 & UART_ERR_WR_MASK) == 0) {
-    ESP_LOGW("oq.uart_filter", "Controller Q %s UART%u: parity/framing error discard did not enable", port_name,
-             uart_num);
-    return;
+    const uint32_t conf0 = REG_READ(UART_CONF0_REG(uart_num));
+    if ((conf0 & UART_ERR_WR_MASK) == 0) {
+      ESP_LOGW("oq.uart_filter", "Controller Q UART%u: parity/framing error discard did not enable", uart_num);
+      continue;
+    }
+
+    ESP_LOGI("oq.uart_filter", "Controller Q UART%u: parity/framing error discard enabled", uart_num);
   }
-
-  ESP_LOGI("oq.uart_filter", "Controller Q %s UART%u: parity/framing error discard enabled", port_name, uart_num);
 }
 
 }  // namespace oq_controller_q_uart
